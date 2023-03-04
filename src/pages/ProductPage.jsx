a0dec6693164ed967c27dacee4e88e0db90c4af1
Saidiislam <saidiislam05@gmail.com>
2023-03-04 20:00:28 +0600

    [REFACTOR] Shifted all the fetchloads to Tanstack Query

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,24 +2,45 @@ import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { Container, ContentBox, List } from "../Components";
 import ItemList from "../Components/ItemList/ItemList";
+import { getX, url } from "../lib/axiosConfig";
 import { pb } from "../lib/pocketbase";
 
 const ProductPage = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    pb.collection("products")
-      .getFullList()
-      .then((res) => setProducts(res));
-  }, [products]);
+  // useEffect(() => {
+  //   pb.collection("products")
+  //     .getFullList()
+  //     .then((res) => setProducts(res));
+  // }, [products]);
+
+  const productQuery = useQuery(
+    ["product"],
+    async () => await fetch(`${url}products/records`).then((res) => res.json())
+  );
+
+  console.log(productQuery.data);
 
   return (
     <ItemList title={`My Products`}>
       {
-        products.map(item => {
-          return <List key={item.key} name={item.title} description={item.desc} price={item.price}/>
-        })
-      }
+        productQuery.isFetching && productQuery.isLoading
+        ? null
+        : productQuery.isLoading 
+        ? (
+          <p>Loading...</p>
+        ) : (
+          productQuery.data.items.map((item) => {
+            return (
+              <List
+                key={item.key}
+                name={item.title}
+                description={item.desc}
+                price={item.price}
+              />
+            );
+          })
+      )}
     </ItemList>
   );
 };
